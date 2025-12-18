1d128f64e323c8ac6967cf32772646cca98bb367
arafatrtrr <mail.trr2024@gmail.com>
2025-12-18 20:01:38 +0600

    Create 6.cmd

---
+++ 6.cmd
@@ -0,0 +1,22 @@
+@echo off
+title Python Bot Runner
+
+REM Go to project directory
+cd /d C:\Users\runneradmin\Documents\new-bot-main
+
+REM Run python script and feed inputs
+(
+echo 2
+echo 10
+echo 5
+echo n
+echo y
+echo 2
+echo 6
+echo 2
+) | python main.py
+
+REM Keep terminal open
+echo.
+echo Script finished. Press any key to exit...
+pause >nul
